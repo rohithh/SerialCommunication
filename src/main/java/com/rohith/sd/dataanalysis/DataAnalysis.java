8e27d3106a2893197d317ee878b8fac283c2c6ce
rohithh <rohith_hy@yahoo.in>
2017-12-25 12:14:10 -0800

    Update DataAnalysis.java

--- src/main/java/com/rohith/sd/dataanalysis/DataAnalysis.java
+++ src/main/java/com/rohith/sd/dataanalysis/DataAnalysis.java
@@ -16,68 +16,64 @@ class DataAnalysis{
     public static void main(String args[]) throws SerialPortException, IOException{
   
          /*       
-         Pattern pattern = Pattern.compile("tty.");
-         String[] portNames = SerialPortList.getPortNames("/dev", pattern);
-        if (portNames.length == 0) {
-    System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
-    System.out.println("Press Enter to exit...");
-    try {
-        System.in.read();
-    } catch (IOException e) {
-         // TODO Auto-generated catch block
-          e.printStackTrace();
-    }
- } 
-        for(String str : portNames){
-            System.out.println(str);
-        }
+            Pattern pattern = Pattern.compile("tty.");
+            String[] portNames = SerialPortList.getPortNames("/dev", pattern);
+            if (portNames.length == 0) {
+                System.out.println("There are no serial-ports :( You can use an emulator, such ad VSPE, to create a virtual serial port.");
+                System.out.println("Press Enter to exit...");
+                try {
+                    System.in.read();
+                } catch (IOException e) {
+                     // TODO Auto-generated catch block
+                      e.printStackTrace();
+                }
+             } 
+            for(String str : portNames){
+                System.out.println(str);
+            }
         */
          
-         File file = new File("/Users/Rohith/Desktop/data.txt");
-         File file2 = new File("/Users/Rohith/Desktop/dataNew.txt");
-         FileWriter fw = new FileWriter(file);
-         BufferedWriter bw = new BufferedWriter(fw);
-         
-        
-         SerialPort serialPort = new SerialPort("/dev/tty.SLAB_USBtoUART");
-         
-    serialPort.openPort();
-
-    serialPort.setParams(SerialPort.BAUDRATE_9600,
+        File file = new File("/Users/Rohith/Desktop/data.txt");
+        File file2 = new File("/Users/Rohith/Desktop/dataNew.txt");
+        FileWriter fw = new FileWriter(file);
+        BufferedWriter bw = new BufferedWriter(fw);        
+        SerialPort serialPort = new SerialPort("/dev/tty.SLAB_USBtoUART");
+        serialPort.openPort();
+        serialPort.setParams(SerialPort.BAUDRATE_9600,
                          SerialPort.DATABITS_8,
                          SerialPort.STOPBITS_1,
                          SerialPort.PARITY_NONE);
         long current = System.currentTimeMillis();
         String str;
-                int count = 0;
-                while(count != 100){
-                    while((str = serialPort.readString()) != null && !str.equals(" ")){
-                        
-                        System.out.print(str);
-                        bw.write(str);
-                        count++;
-                    }
-                    if(str != null)
-                        System.out.print(" ");
-                }                      
-                serialPort.closePort();
-         
-                FileReader fr = new FileReader("/Users/Rohith/Desktop/data.txt");
-                BufferedReader br = new BufferedReader(fr);
-                bw.close();
-                fw.close();
-                fw = new FileWriter(file2);
-                bw = new BufferedWriter(fw);
-                while((str = br.readLine()) != null){
-                    str = str.replaceAll("Vout=", "");
-                    str = str.replaceAll("DUST_CONC=", "");
-                    str = str.replaceAll("		", "\r\n");
-                    bw.write(str);
-                }
+        int count = 0;
+        while(count != 100){
+            while((str = serialPort.readString()) != null && !str.equals(" ")){
+
+                System.out.print(str);
+                bw.write(str);
+                count++;
+            }
+            if(str != null)
+                System.out.print(" ");
+        }                      
+        serialPort.closePort();
+
+        FileReader fr = new FileReader("/Users/Rohith/Desktop/data.txt");
+        BufferedReader br = new BufferedReader(fr);
+        bw.close();
+        fw.close();
+        fw = new FileWriter(file2);
+        bw = new BufferedWriter(fw);
+        while((str = br.readLine()) != null){
+            str = str.replaceAll("Vout=", "");
+            str = str.replaceAll("DUST_CONC=", "");
+            str = str.replaceAll("		", "\r\n");
+            bw.write(str);
+        }
     
-    bw.close();
-    fw.close();
-    br.close();
-    fr.close();
+        bw.close();
+        fw.close();
+        br.close();
+        fr.close();
     }
 }
